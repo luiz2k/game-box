5e27bf2069178b639f58dd499481fda72dd509bd
luiz2k <luizteles934@gmail.com>
2025-01-06 15:32:52 -0300

    Definir cores da aplicação

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,18 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        "black-1": "#141412",
+        "black-2": "#32322F",
+        "black-3": "#3A3A35",
+        "white-1": "#F3F4F6",
+        "accent-1": "#0284C7",
+        "accent-2": "#036BA1",
+        "accent-3": "#075A85",
+      },
+    },
+  },
   plugins: [],
 } satisfies Config;

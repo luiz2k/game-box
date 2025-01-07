d752cf2173d21c092aba5ddf6bf535fd7f4b1e9c
luiz2k <luizteles934@gmail.com>
2025-01-07 17:59:58 -0300

    Criar formulário de login

---
+++ src/modules/shared/components/Forms/LoginForm/LoginForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useFormStore } from "../../../stores/formStore";
+import { Button } from "../../Button/Button";
+import {
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  DialogHeaderDesc,
+  DialogHeaderTitle,
+  DialogWrapping,
+} from "../../Dialog/Dialog";
+import { FormInput } from "../../Input/Input";
+
+export function LoginForm() {
+  const { loginFormIsOpen, changeForm } = useFormStore();
+
+  return (
+    <>
+      {loginFormIsOpen && (
+        <DialogWrapping>
+          <DialogHeader>
+            <DialogHeaderTitle>Registro</DialogHeaderTitle>
+            <DialogHeaderDesc>
+              Entre com seu e-mail e senha para acessar sua conta.
+            </DialogHeaderDesc>
+          </DialogHeader>
+
+          <DialogBody>
+            <FormInput label="E-mail" placeholder="@" width="full" />
+            <FormInput label="Senha" placeholder="kwjd451q" width="full" />
+          </DialogBody>
+
+          <DialogFooter>
+            <Button
+              variant="ghost"
+              width="full"
+              type="button"
+              onClick={changeForm}
+            >
+              Registrar
+            </Button>
+            <Button variant="primary" width="full" type="submit">
+              Entrar
+            </Button>
+          </DialogFooter>
+        </DialogWrapping>
+      )}
+    </>
+  );
+}

06b87049477a2ca505b0dff3de01789b2d58e971
luiz2k <luizteles934@gmail.com>
2025-01-08 19:40:51 -0300

    Fazer diversas alterações no âncora de navegação
    - Definir o estilo através de Tailwind Variants
    - Verificar se deve conter highlight através de uma função
    - Fazer verificação de highlight também nas subrotas

--- src/modules/shared/components/NavAnchor/NavAnchor.tsx
+++ src/modules/shared/components/NavAnchor/NavAnchor.tsx
@@ -2,22 +2,43 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { tv } from "tailwind-variants";
 
 type NavAnchorProps = {
   href: string;
   children: string;
 };
 
+const NavAnchorTv = tv({
+  base: "inline-block rounded-full px-4 py-2 text-sm text-white-1 duration-200 hover:bg-accent-2",
+  variants: {
+    highlight: {
+      true: "bg-accent-1",
+    },
+  },
+});
+
+// Verifica se o `href` corresponde ao `pathname`
+const handleAnchorHighlight = (pathname: string, href: string) => {
+  const hrefFormatted = href.slice(1); // Remove o "/" do `href`
+  const pathnameSplited = pathname.split("/"); // Quebra o `pathname` em um array
+
+  const includes = pathnameSplited.includes(hrefFormatted); // Verifica se no array tem o `hrefFormatted`
+
+  // Se o `href` for "/" faz a comparação entre o `pathname` e o `href` normalmente
+  // Se não, usa a verificação de `includes`
+  const currentPage = href === "/" ? href === pathname : includes;
+
+  return currentPage;
+};
+
 export function NavAnchor({ href, children }: NavAnchorProps) {
   const pathname = usePathname();
-  const currentPage = href === pathname;
+
+  const anchorHighlight = handleAnchorHighlight(pathname, href);
 
   return (
-    <Link
-      href={href}
-      data-current-page={currentPage}
-      className="hover:bg-accent-2 data-[current-page=true]:bg-accent-1 inline-block rounded-full px-4 py-2 text-sm text-white-1 duration-200"
-    >
+    <Link href={href} className={NavAnchorTv({ highlight: anchorHighlight })}>
       {children}
     </Link>
   );

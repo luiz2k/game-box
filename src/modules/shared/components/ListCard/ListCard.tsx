2d541878e0229afb5c3ec20583a5d07f0ae421ce
luiz2k <luizteles934@gmail.com>
2025-01-08 17:03:26 -0300

    Melhorar responsitivdade e a possibilidade de adicionar icone

--- src/modules/shared/components/ListCard/ListCard.tsx
+++ src/modules/shared/components/ListCard/ListCard.tsx
@@ -1,15 +1,29 @@
+import { LucideIcon } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+
 type ListCardProps = {
   title: string;
+  icon?: LucideIcon;
+  className?: string;
 };
 
-export function ListCard({ title }: ListCardProps) {
+export function ListCard({ title, icon: Icon, className }: ListCardProps) {
   return (
-    <div className="group w-56 space-y-2 text-white-1">
-      <div className="group-[&:hover]:bg-black-3 flex h-[18.75rem] items-center justify-center rounded-2xl bg-black-2 text-6xl duration-200">
-        {title[0].toUpperCase()}
+    <div className="group space-y-2 text-white-1">
+      <div
+        className={twMerge(
+          "flex h-[18.75rem] items-center justify-center rounded-2xl bg-black-2 text-6xl duration-200 group-[&:hover]:bg-black-3",
+          className,
+        )}
+      >
+        {Icon ? (
+          <Icon className="size-[60px]" />
+        ) : (
+          <>{title[0].toUpperCase()}</>
+        )}
       </div>
 
-      <h2 className="text-center">{title}</h2>
+      <h2 className="text-center text-sm">{title}</h2>
     </div>
   );
 }

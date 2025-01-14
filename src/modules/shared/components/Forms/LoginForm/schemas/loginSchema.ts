673d834912deba08bc2e58504dbda65f262daacc
luiz2k <luizteles934@gmail.com>
2025-01-14 17:59:33 -0300

    Criar esquema de validação para o login

---
+++ src/modules/shared/components/Forms/LoginForm/schemas/loginSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .string({ message: "O dado deve ser do tipo string." })
+    .min(1, { message: "Informe um e-mail." }),
+  password: z
+    .string({ message: "O dado deve ser do tipo string." })
+    .min(1, { message: "Informe uma senha." }),
+});

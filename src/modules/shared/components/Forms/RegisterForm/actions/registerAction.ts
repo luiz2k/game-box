07c4d84cdc3b1c25b1af8b2e9c92a422056573b7
luiz2k <luizteles934@gmail.com>
2025-01-14 14:42:51 -0300

    Desenvolver server action responsável por criar um novo usuário

---
+++ src/modules/shared/components/Forms/RegisterForm/actions/registerAction.ts
@@ -0,0 +1,71 @@
+"use server";
+
+import { prisma } from "@/modules/lib/prisma/prisma";
+import { hash } from "bcryptjs";
+import { registerSchema } from "../validations/registerSchema";
+
+// Ação de registrar um usuário
+export async function registerAction(_prev: unknown, formData: FormData) {
+  try {
+    // Converte os dados do formulário em um objeto
+    const data = Object.fromEntries(formData);
+
+    // Valida os dados usando o esquema definido
+    const isValid = registerSchema.safeParse(data);
+
+    if (!isValid.success) {
+      return {
+        messages: {
+          error: "Por favor, preencha todos os campos corretamente.",
+        },
+        inputErrors: isValid.error?.flatten().fieldErrors, // Retorna os erros de validação
+        inputValues: data, // Retorna os valores do formulário
+      };
+    }
+
+    // Desestrutura os dados do formulário
+    const { username, email, password } = isValid.data;
+
+    // Verifica se o usuário já existe no banco
+    const userExist = await prisma.user.count({
+      where: {
+        email,
+      },
+    });
+
+    if (userExist > 0) {
+      return {
+        messages: {
+          error: "E-mail já cadastrado.",
+        },
+        inputValues: data, // Retorna os valores do formulário
+      };
+    }
+
+    // Cria o hash da senha
+    const hashedPassword = await hash(password, 10);
+
+    // Cria o usuário no banco de dados
+    await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      messages: {
+        success: "Cadastro realizado com sucesso.",
+      },
+    };
+  } catch (error) {
+    console.error("Ocorreu um erro inesperado:", error);
+
+    return {
+      messages: {
+        error: "Ocorreu um erro inesperado. Por favor, tente novamente.",
+      },
+    };
+  }
+}

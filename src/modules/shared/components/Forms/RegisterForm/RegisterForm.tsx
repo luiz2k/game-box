0704fbc5ee64d6c534ce9d2defd17c3b0f29f6f6
luiz2k <luizteles934@gmail.com>
2025-01-14 14:55:42 -0300

    Fazer a integração no furmulário de registro

--- src/modules/shared/components/Forms/RegisterForm/RegisterForm.tsx
+++ src/modules/shared/components/Forms/RegisterForm/RegisterForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useActionState } from "react";
 import { useFormStore } from "../../../stores/formStore";
 import { Button } from "../../Button/Button";
 import {
@@ -11,28 +12,78 @@ import {
   DialogWrapping,
 } from "../../Dialog/Dialog";
 import { FormInput } from "../../Input/Input";
+import { registerAction } from "./actions/registerAction";
 
 export function RegisterForm() {
   const { handleRegisterForm, registerFormIsOpen, changeForm } = useFormStore();
 
+  const [formState, formAction, isPending] = useActionState(
+    registerAction,
+    null,
+  );
+
   return (
     <>
       {registerFormIsOpen && (
-        <DialogWrapping close={handleRegisterForm}>
+        <DialogWrapping close={handleRegisterForm} action={formAction}>
           <DialogHeader>
             <DialogHeaderTitle>Registro</DialogHeaderTitle>
+
             <DialogHeaderDesc>
-              Crie uma conta e organize seus jogos.
+              {/* Exibe mensagens de sucesso ou erro do formulário */}
+              {formState?.messages && (
+                <>
+                  {formState.messages.success && (
+                    <p className="text-green-600">
+                      {formState.messages.success}
+                    </p>
+                  )}
+
+                  {formState.messages.error && (
+                    <p className="text-red-600">{formState.messages.error}</p>
+                  )}
+                </>
+              )}
+
+              {/* Se não houver mensagens, exibe a menssagem padrão */}
+              {!formState?.messages && (
+                <p>Crie uma conta e organize seus jogos.</p>
+              )}
             </DialogHeaderDesc>
           </DialogHeader>
 
           <DialogBody>
-            <FormInput label="Usuário" placeholder="Exemplo" width="full" />
-            <FormInput label="E-mail" placeholder="@" width="full" />
-            <FormInput label="Senha" placeholder="kwjd451q" width="full" />
+            <FormInput
+              label="Usuário"
+              name="username"
+              placeholder="Exemplo"
+              error={formState?.inputErrors?.username?.toString()}
+              defaultValue={formState?.inputValues?.username.toString()}
+              autoFocus
+              width="full"
+            />
+            <FormInput
+              label="E-mail"
+              name="email"
+              placeholder="@"
+              error={formState?.inputErrors?.email?.toString()}
+              defaultValue={formState?.inputValues?.email.toString()}
+              width="full"
+            />
+            <FormInput
+              label="Senha"
+              name="password"
+              placeholder="kwjd451q"
+              error={formState?.inputErrors?.password?.toString()}
+              defaultValue={formState?.inputValues?.password.toString()}
+              width="full"
+            />
             <FormInput
               label="Confirmar senha"
+              name="confirmPassword"
               placeholder="kwjd451q"
+              error={formState?.inputErrors?.confirmPassword?.toString()}
+              defaultValue={formState?.inputValues?.confirmPassword.toString()}
               width="full"
             />
           </DialogBody>
@@ -46,7 +97,12 @@ export function RegisterForm() {
             >
               Entrar
             </Button>
-            <Button variant="primary" width="full" type="submit">
+            <Button
+              variant="primary"
+              width="full"
+              type="submit"
+              disabled={isPending}
+            >
               Registrar
             </Button>
           </DialogFooter>

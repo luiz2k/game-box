18c1cb737e0ca78cc18ec50a0c8e0fe798f1a0e0
luiz2k <luizteles934@gmail.com>
2025-01-15 18:49:00 -0300

    Criar função para buscar o usuário pelo ID

--- src/modules/shared/lib/prisma/prisma.ts
+++ src/modules/shared/lib/prisma/prisma.ts
@@ -5,3 +5,18 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// Obtém os dados do usuário pelo ID
+export async function getUserById(id: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      username: true,
+      plan: true,
+    },
+  });
+
+  return user;
+}

13dde75802bd124e1133f08b93ed26b8a9b3ec92
luiz2k <luizteles934@gmail.com>
2025-01-15 18:41:48 -0300

    Configurar os callbacks

--- src/auth.ts
+++ src/auth.ts
@@ -22,4 +22,21 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      // Se o usuário acabou de logar, adicione os dados do usuário ao token
+      if (user) {
+        token.user = user;
+      }
+
+      // Mantém o token anterior
+      return token;
+    },
+    session: async ({ session, token }) => {
+      // Adicione os dados do token à sessão
+      session.user = token.user;
+
+      return session;
+    },
+  },
 });

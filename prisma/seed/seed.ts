18981dcc6fec84e9d8941b1242a635de75cb229c
luiz2k <luizteles934@gmail.com>
2025-01-16 17:26:15 -0300

    Comentar código

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,16 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import data from "./data.json";
 
+// Cria uma instância do Prisma
 const prisma = new PrismaClient({
   log: ["query", "info", "warn", "error"],
 });
 
+// Faz a formatação da data dos games
 const games = data.map(({ release_date, ...rest }) => ({
   release_date: new Date(release_date),
   ...rest,
 }));
 
 (async () => {
+  // Insere os jogos na tabela
   await prisma.game.createMany({
     data: games,
   });

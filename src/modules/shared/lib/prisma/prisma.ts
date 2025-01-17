705051bac480f80df41e1597821f5f5ffa1980c0
luiz2k <luizteles934@gmail.com>
2025-01-17 11:23:37 -0300

    Criar funções para manipular as caixas padrão

--- src/modules/shared/lib/prisma/prisma.ts
+++ src/modules/shared/lib/prisma/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Box } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -37,4 +37,57 @@ export async function findGameById(id: number) {
   });
 
   return game;
+}
+
+type FindStandardBoxByName = {
+  userId: number;
+  gameId?: number;
+  box?: Box;
+};
+
+// Busca as caixas padrão pelo ID do usuário
+export async function findAllStandardBoxByUserId(data: FindStandardBoxByName) {
+  const standardBox = await prisma.standardBox.findMany({
+    where: {
+      userId: data.userId,
+      gameId: data.gameId,
+      box: data.box,
+    },
+  });
+
+  return standardBox;
+}
+
+type AddGameToBox = {
+  userId: number;
+  gameId: number;
+  box: Box;
+};
+
+// Adiciona o jogo em uma caixa padrão
+export async function addGameToStandardBox(data: AddGameToBox) {
+  await prisma.standardBox.create({
+    data: {
+      userId: data.userId,
+      gameId: data.gameId,
+      box: data.box,
+    },
+  });
+}
+
+type RemoveGameToBox = {
+  userId: number;
+  gameId: number;
+  box: Box;
+};
+
+// Remove o jogo de uma caixa padrão
+export async function removeGameToStandardBox(data: RemoveGameToBox) {
+  await prisma.standardBox.deleteMany({
+    where: {
+      userId: data.userId,
+      gameId: data.gameId,
+      box: data.box,
+    },
+  });
 }

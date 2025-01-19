23fd1e2a698dab4b30cc5f8a99da57935a0f5413
luiz2k <luizteles934@gmail.com>
2025-01-19 13:35:36 -0300

    Melhorar informações exibidas na página e código
    - Exibir o nome da caixa
    - Exibir menssagem se nenhum jogo foi listado
    - Melhorar comentário do código

--- src/modules/pages/CustomBoxPage/CustomBoxPage.tsx
+++ src/modules/pages/CustomBoxPage/CustomBoxPage.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import {
   GameCardBody,
   GameCardBodyAction,
@@ -7,13 +8,14 @@ import {
   GameCardImage,
   GameCardWarapping,
 } from "@/modules/shared/components/GameCard/GameCard";
+import { PageTitle } from "@/modules/shared/components/PageTitle/PageTitle";
 import {
   findAllGameListedInBox,
   findCustomBoxByUserId,
 } from "@/modules/shared/lib/prisma/prisma";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 import { DialogForm } from "./components/DialogForm/DialogForm";
-import { auth } from "@/auth";
 import { GameCardCustomAction } from "./components/GameCardCustomAction/GameCardCustomAction";
 
 type CustomBoxPageProps = {
@@ -25,61 +27,80 @@ type CustomBoxPageProps = {
 export async function CustomBoxPage({ params }: CustomBoxPageProps) {
   const { box: boxId } = await params;
 
+  // Ontém o ID do usuário através da sessão
   const session = await auth();
   const userId = session?.user?.id;
 
+  // Busca informações sobre a caixa
   const customBox = await findCustomBoxByUserId(Number(boxId));
 
+  // Se não encontrar a caixa, retorna 404
+  if (!customBox) {
+    notFound();
+  }
+
+  // Busca todos os jogos listados na caixa
   const games = await findAllGameListedInBox(Number(boxId));
 
   return (
     <section className="grid gap-10">
-      <div className="space-y-1">
-        <h1 className="text-center text-4xl font-bold">{customBox?.name}</h1>
-        <p className="text-center">
-          Jogos listados da caixa{" "}
-          <span className="font-bold">{customBox?.name}</span>.
-        </p>
-      </div>
+      <PageTitle
+        title={customBox?.name}
+        desc={
+          <>
+            Jogos listados na caixa{" "}
+            <span className="font-bold">{customBox?.name}</span>.
+          </>
+        }
+      />
 
       <div className="grid grid-cols-[repeat(auto-fit,_minmax(245px,_1fr))] gap-4">
-        {games.map((game) => (
-          <Link
-            key={game.Game.id}
-            title={game.Game.title}
-            href={`/jogos/${game.Game.id}`}
-          >
-            <GameCardWarapping>
-              <GameCardImage imgSrc={game.Game.cover} alt={game.Game.title} />
-
-              <GameCardBody>
-                <GameCardBodyHeader>
-                  <GameCardBodyHeaderTitle title={game.Game.title} />
-                  <GameCardBodyHeaderDesc>
-                    <p>{game.Game.genre.join(", ")}</p>
-                    <p>
-                      {/* Faz a formatação da data para pt-BR. EX: 01 de janeiro de 2022 */}
-                      {new Date(game.Game.release_date).toLocaleDateString(
-                        "pt-BR",
-                        {
-                          day: "numeric",
-                          month: "long",
-                          year: "numeric",
-                        },
-                      )}
-                    </p>
-                  </GameCardBodyHeaderDesc>
-                </GameCardBodyHeader>
-                <GameCardBodyAction>
-                  <GameCardCustomAction
-                    title={game.Game.title}
-                    gameId={game.Game.id}
+        {games.length > 0 ? (
+          <>
+            {games.map((game) => (
+              <Link
+                key={game.Game.id}
+                title={game.Game.title}
+                href={`/jogos/${game.Game.id}`}
+              >
+                <GameCardWarapping>
+                  <GameCardImage
+                    imgSrc={game.Game.cover}
+                    alt={game.Game.title}
                   />
-                </GameCardBodyAction>
-              </GameCardBody>
-            </GameCardWarapping>
-          </Link>
-        ))}
+
+                  <GameCardBody>
+                    <GameCardBodyHeader>
+                      <GameCardBodyHeaderTitle title={game.Game.title} />
+                      <GameCardBodyHeaderDesc>
+                        <p>{game.Game.genre.join(", ")}</p>
+                        <p>
+                          {/* Faz a formatação da data para pt-BR. EX: 01 de janeiro de 2022 */}
+                          {new Date(game.Game.release_date).toLocaleDateString(
+                            "pt-BR",
+                            {
+                              day: "numeric",
+                              month: "long",
+                              year: "numeric",
+                            },
+                          )}
+                        </p>
+                      </GameCardBodyHeaderDesc>
+                    </GameCardBodyHeader>
+                    <GameCardBodyAction>
+                      <GameCardCustomAction
+                        title={game.Game.title}
+                        gameId={game.Game.id}
+                      />
+                    </GameCardBodyAction>
+                  </GameCardBody>
+                </GameCardWarapping>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="text-center">Nenhum jogo foi listado nesta caixa.</p>
+        )}
       </div>
 
       <DialogForm customBox={customBox!} userId={Number(userId)} />

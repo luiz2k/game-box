5be20159b89345e08098cb31835d9b9008b6d11a
luiz2k <luizteles934@gmail.com>
2025-01-19 15:24:26 -0300

    Criar função para obter os dados da sessão do usuário

---
+++ src/modules/shared/utils/session.ts
@@ -0,0 +1,31 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+// Lida com a obtenção da sessão do usuário e com o controle das rotas privadas
+export async function authSession() {
+  // Obtém os dados da sessão
+  const session = await auth();
+
+  // Se os dados não for encontrados, redireciona para a página inicial
+  if (!session) {
+    return redirect("/");
+  }
+
+  if (!session?.expires) {
+    return redirect("/");
+  }
+
+  if (!session?.user?.id) {
+    return redirect("/");
+  }
+
+  if (!session?.user?.username) {
+    return redirect("/");
+  }
+
+  return {
+    expires: session.expires,
+    id: +session.user.id,
+    username: session.user.username,
+  };
+}

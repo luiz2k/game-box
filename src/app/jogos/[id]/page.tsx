d82e479b885a9b8e480461844a421e93fbfff3e0
luiz2k <luizteles934@gmail.com>
2025-01-20 18:11:23 -0300

    Implementar ISR na página de jogo

--- src/app/jogos/[id]/page.tsx
+++ src/app/jogos/[id]/page.tsx
@@ -1,3 +1,13 @@
 import { GamePage } from "@/modules/pages/GamePage/GamePage";
+import { findAllGames } from "@/modules/shared/lib/prisma/prisma";
+
+// Gera as rotas dinâmicas de forma estática
+export async function generateStaticParams() {
+  const games = await findAllGames();
+
+  return games.map((game) => ({
+    id: String(game.id),
+  }));
+}
 
 export default GamePage;

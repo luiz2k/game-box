c7838861aba146571e1e5f0ccaa04dd9cd890dd2
luiz2k <luizteles934@gmail.com>
2025-01-23 01:43:14 -0300

    Criar as caixas padrão através da constant

--- src/modules/pages/ProfilePage/components/Boxes/components/StandardBoxes/StandardBoxes.tsx
+++ src/modules/pages/ProfilePage/components/Boxes/components/StandardBoxes/StandardBoxes.tsx
@@ -5,6 +5,7 @@ import {
   BoxCardTitle,
   BoxCardWrapper,
 } from "../../../BoxCard/BoxCard";
+import { standardBoxes } from "@/modules/shared/utils/standardBoxes";
 
 export function StandardBoxes() {
   return (
@@ -12,42 +13,20 @@ export function StandardBoxes() {
       <h2 className="text-2xl font-bold">Caixas padrão</h2>
 
       <div className="grid grid-cols-[repeat(auto-fit,_minmax(12.5rem,_1fr))] gap-4">
-        <Link href={`/perfil/caixa/padrao/favorite`}>
-          <BoxCardWrapper>
-            <BoxCardHeader>
-              <BoxCardHeaderContent title={"Favorito"} />
-            </BoxCardHeader>
+        {standardBoxes.map((box) => (
+          <Link
+            key={box.box}
+            href={`/perfil/caixa/padrao/${box.box.toLowerCase()}`}
+          >
+            <BoxCardWrapper>
+              <BoxCardHeader>
+                <BoxCardHeaderContent title={box.name} />
+              </BoxCardHeader>
 
-            <BoxCardTitle>Favorito</BoxCardTitle>
-          </BoxCardWrapper>
-        </Link>
-        <Link href={`/perfil/caixa/padrao/playing`}>
-          <BoxCardWrapper>
-            <BoxCardHeader>
-              <BoxCardHeaderContent title="Jogando" />
-            </BoxCardHeader>
-
-            <BoxCardTitle>Jogando</BoxCardTitle>
-          </BoxCardWrapper>
-        </Link>
-        <Link href={`/perfil/caixa/padrao/abandoned`}>
-          <BoxCardWrapper>
-            <BoxCardHeader>
-              <BoxCardHeaderContent title="Abandonado" />
-            </BoxCardHeader>
-
-            <BoxCardTitle>Abandonado</BoxCardTitle>
-          </BoxCardWrapper>
-        </Link>
-        <Link href={`/perfil/caixa/padrao/finished`}>
-          <BoxCardWrapper>
-            <BoxCardHeader>
-              <BoxCardHeaderContent title="Finalizado" />
-            </BoxCardHeader>
-
-            <BoxCardTitle>Finalizado</BoxCardTitle>
-          </BoxCardWrapper>
-        </Link>
+              <BoxCardTitle>{box.name}</BoxCardTitle>
+            </BoxCardWrapper>
+          </Link>
+        ))}
       </div>
     </div>
   );

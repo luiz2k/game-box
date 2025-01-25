93cd15e9e52b8ddf3c9d16767dd85a598ff47b85
luiz2k <luizteles934@gmail.com>
2025-01-25 16:33:23 -0300

    Desacoplar código

--- src/modules/pages/ProfilePage/components/SignatureInfo/SignatureInfo.tsx
+++ src/modules/pages/ProfilePage/components/SignatureInfo/SignatureInfo.tsx
@@ -1,54 +1,28 @@
 import { SubscriptionCard } from "@/modules/shared/components/SubscriptionCard/SubscriptionCard";
-import { findAllCustomBoxByUserId } from "@/modules/shared/lib/prisma/customBox";
-import { getPlanInfos } from "@/modules/shared/utils/plains";
-import { twMerge } from "tailwind-merge";
+import { BenefitsInfo } from "./components/BenefitsInfo/BenefitsInfo";
+import { SubscriptionInfo } from "./components/SubscriptionInfo/SubscriptionInfo";
+import { auth } from "@/auth";
+import { findUserById } from "@/modules/shared/lib/prisma/user";
 
-type SignatureInfoProps = {
-  plan: string;
-  session: {
-    expires: string;
-    id: number;
-    username: string;
-  };
-};
+export async function SignatureInfo() {
+  const session = await auth();
 
-export async function SignatureInfo({ plan, session }: SignatureInfoProps) {
-  // Obtém as informações do plano do usuário
-  const planInfos = getPlanInfos(plan);
+  if (!session) {
+    return null;
+  }
 
-  // Obtém as caixas do usuário
-  const userBoxes = await findAllCustomBoxByUserId({
-    userId: session.id,
+  const user = await findUserById({
+    userId: +session.user.id,
   });
 
   return (
     <div className="flex flex-col gap-4 min-[962px]:flex-row">
-      <div
-        className={twMerge(
-          "min-w-[18.75rem] space-y-5 rounded-2xl bg-black-2 p-4",
-          planInfos?.name === "Premium" && "w-full",
-        )}
-      >
-        <h2 className="text-2xl font-bold">Sua assinatura</h2>
+      <BenefitsInfo />
 
-        <div className="space-y-1 text-sm">
-          <p>
-            <span className="font-bold">Plano atual:</span> {planInfos?.name}
-          </p>
-          <p>
-            <span className="font-bold">Suas caixas:</span> {userBoxes.length} /{" "}
-            {planInfos?.boxLimit}
-          </p>
-          <p>
-            <span className="font-bold">Máximo de jogos por caixa:</span>{" "}
-            {planInfos?.gameLimit}
-          </p>
-        </div>
-      </div>
+      <SubscriptionInfo />
 
-      <div className="flex-grow">
-        {planInfos?.name === "Free" && <SubscriptionCard />}
-      </div>
+      {/* Só exibe o cartão de assinatura se o plano do usuário for gratuito */}
+      {user?.plan === "Free" && <SubscriptionCard />}
     </div>
   );
 }

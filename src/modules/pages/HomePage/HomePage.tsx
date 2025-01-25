00f55540c88baa7aec8756199a0e3d8e991a04a5
luiz2k <luizteles934@gmail.com>
2025-01-25 18:57:53 -0300

    Corrigir exibição do SubscriptionCard

--- src/modules/pages/HomePage/HomePage.tsx
+++ src/modules/pages/HomePage/HomePage.tsx
@@ -1,15 +1,30 @@
 import { SubscriptionCard } from "@/modules/shared/components/SubscriptionCard/SubscriptionCard";
 import { Cover } from "./components/Cover/Cover";
 import { PopularGames } from "./components/PopularGames/PopularGames";
+import { auth } from "@/auth";
+import { prisma } from "@/modules/shared/lib/prisma/prisma";
 
-export function HomePage() {
+export async function HomePage() {
+  // Obtém a sessão do usuário
+  const session = await auth();
+
+  // Se o usuário estiver autenticado, obtem os dados do usuário
+  const user =
+    session &&
+    (await prisma.user.findUnique({
+      where: {
+        id: +session?.user.id,
+      },
+    }));
+
   return (
     <>
       <Cover />
 
       <PopularGames />
 
-      <SubscriptionCard />
+      {/* Só exibe o cartão de assinatura se o plano do usuário for gratuito ou se o usuário nao estiver autenticado */}
+      {(user?.plan === "Free" || !user) && <SubscriptionCard />}
     </>
   );
 }

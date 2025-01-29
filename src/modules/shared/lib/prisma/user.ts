3b843287ac616013353515f3543067ec92e9080a
luiz2k <luizteles934@gmail.com>
2025-01-29 14:48:35 -0300

    Criar novas funções CRUD para manipular a tabela User

--- src/modules/shared/lib/prisma/user.ts
+++ src/modules/shared/lib/prisma/user.ts
@@ -1,11 +1,11 @@
 import { prisma } from "./prisma";
 
-type findUserById = {
+type FindUserById = {
   userId: number;
 };
 
 // Obtém os dados do usuário pelo ID
-export async function findUserById({ userId }: findUserById) {
+export async function findUserById({ userId }: FindUserById) {
   const user = await prisma.user.findUnique({
     where: {
       id: userId,
@@ -17,4 +17,65 @@ export async function findUserById({ userId }: findUserById) {
   });
 
   return user;
+}
+
+type FindUserByEmail = {
+  email: string;
+};
+
+// Obtém os dados do usuário pelo E-mail
+export async function findUserByEmail({ email }: FindUserByEmail) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+    omit: {
+      password: true,
+    },
+  });
+
+  return user;
+}
+
+type FindUserByStripeCustomerId = {
+  stripeCustomerId: string;
+};
+
+// Obtém os dados do usuário pelo seu Id no Stripe
+export async function findUserByStripeCustomerId({
+  stripeCustomerId,
+}: FindUserByStripeCustomerId) {
+  const user = await prisma.user.findUnique({
+    where: {
+      stripeCustomerId: stripeCustomerId,
+    },
+    omit: {
+      password: true,
+    },
+  });
+
+  return user;
+}
+
+type UpdatedUserPlan = {
+  userId: number;
+  SubscriptionId: string;
+  plan: string;
+};
+
+// Atualiza o plano do usuário
+export async function updatedUserPlan({
+  userId,
+  SubscriptionId,
+  plan,
+}: UpdatedUserPlan) {
+  await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      stripeSubscriptionId: SubscriptionId,
+      plan: plan,
+    },
+  });
 }

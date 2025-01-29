2acc0ef7db6f5ae0a27a1ca7993b9db7b4b93af5
luiz2k <luizteles934@gmail.com>
2025-01-29 18:40:38 -0300

    Melhorar escrita e comentários do código

--- src/modules/pages/HomePage/HomePage.tsx
+++ src/modules/pages/HomePage/HomePage.tsx
@@ -1,30 +1,28 @@
+import { auth } from "@/auth";
 import { SubscriptionCard } from "@/modules/shared/components/SubscriptionCard/SubscriptionCard";
+import { findUserById } from "@/modules/shared/lib/prisma/user";
 import { Cover } from "./components/Cover/Cover";
 import { PopularGames } from "./components/PopularGames/PopularGames";
-import { auth } from "@/auth";
-import { prisma } from "@/modules/shared/lib/prisma/prisma";
 
 export async function HomePage() {
   // Obtém a sessão do usuário
   const session = await auth();
 
-  // Se o usuário estiver autenticado, obtem os dados do usuário
-  const user =
-    session &&
-    (await prisma.user.findUnique({
-      where: {
-        id: +session?.user.id,
-      },
-    }));
+  let user = null;
+
+  // Se o usuário estiver autenticado, obtém os dados do usuário
+  if (session && session?.user?.id) {
+    user = await findUserById({
+      userId: +session.user.id,
+    });
+  }
 
   return (
     <>
       <Cover />
-
       <PopularGames />
-
-      {/* Só exibe o cartão de assinatura se o plano do usuário for gratuito ou se o usuário nao estiver autenticado */}
-      {(user?.plan === "Free" || !user) && <SubscriptionCard />}
+      {/* Exibe o cartão de assinatura apenas se o plano do usuário for gratuito ou se não estiver autenticado */}
+      {!user || user.plan === "Free" ? <SubscriptionCard /> : null}
     </>
   );
 }
